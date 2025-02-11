56d29c4428f9e34eeb331d87e190ac82cc7b8689
github2022Oug <walnutr20912@gmail.com>
2025-02-11 14:24:24 +0900

    first commit

---
+++ src/test/java/com/example/roadmap2_0/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadmap2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
